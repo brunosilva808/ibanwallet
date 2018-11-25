8316fc04403adba8f7ce4d05a336305dd29b47f5
brunosilva808 <bsimoesmail@gmail.com>
2018-11-25 01:15:34 +0000

    * failed Skeleton pod implementation

--- iBanwallet/Controller/ViewController.swift
+++ iBanwallet/Controller/ViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    var shownIndexes : [IndexPath] = []
     private var finishedLoadingInitialTableCells = false
-    var gists:[RealmGist] = []
-    var detailViewController: DetailViewController!
+    private var gists:[RealmGist] = []
+    private var detailViewController: DetailViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +38,11 @@ class ViewController: UITableViewController {
 }
 
 extension ViewController {
-    
-    
-    
+
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        
+
         var lastInitialDisplayableCell = false
-        
+
         //change flag as soon as last displayable cell is being loaded (which will mean table has initially loaded)
         if gists.count > 0 && !finishedLoadingInitialTableCells {
             if let indexPathsForVisibleRows = tableView.indexPathsForVisibleRows,
@@ -53,41 +50,23 @@ extension ViewController {
                 lastInitialDisplayableCell = true
             }
         }
-        
+
         if !finishedLoadingInitialTableCells {
-            
+
             if lastInitialDisplayableCell {
                 finishedLoadingInitialTableCells = true
             }
-            
+
             //animates the cell as it is being displayed for the first time
             cell.transform = CGAffineTransform(translationX: 0, y: cell.frame.height/2)
             cell.alpha = 0
-            
+
             UIView.animate(withDuration: 0.5, delay: 0.05*Double(indexPath.row), options: [.curveEaseInOut], animations: {
                 cell.transform = CGAffineTransform(translationX: 0, y: 0)
                 cell.alpha = 1
             }, completion: nil)
         }
     }
-    
-//    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        if (shownIndexes.contains(indexPath) == false) {
-//            shownIndexes.append(indexPath)
-//
-//            cell.transform = CGAffineTransform(translationX: 0, y: cell.frame.height)
-//            cell.layer.shadowColor = UIColor.black.cgColor
-//            cell.layer.shadowOffset = CGSize(width: 10, height: 10)
-//            cell.alpha = 0
-//
-//            UIView.beginAnimations("rotation", context: nil)
-//            UIView.setAnimationDuration(0.5)
-//            cell.transform = CGAffineTransform(translationX: 0, y: 0)
-//            cell.alpha = 1
-//            cell.layer.shadowOffset = CGSize(width: 0, height: 0)
-//            UIView.commitAnimations()
-//        }
-//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return gists.count
